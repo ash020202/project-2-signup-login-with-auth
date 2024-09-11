8d6e0b0ad8546f8ddb2ed359ab4647d9a544883b
ash020202 <130071750+ash020202@users.noreply.github.com>
2024-09-11 12:10:17 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import cron from 'node-cron';
+import axios from 'axios';
 import dotenv from "dotenv";
 import connectDB from "./database/db.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -12,6 +14,24 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", userRoutes);
+
+app.get('/health', (req, res) => {
+    res.send('Server is up and running!');
+});
+
+const pingUrl = 'https://project-2-signup-login-with-auth-backend.onrender.com/health';
+
+cron.schedule('0 0 * * *', () => {
+    console.log('Pinging server to keep it active...');
+
+    axios.get(pingUrl)
+        .then(response => {
+            console.log('Ping successful:', response.status);
+        })
+        .catch(error => {
+            console.error('Error pinging the server:', error);
+        });
+});
 
 app.listen(port, () => {
   console.log("server is running on port", port);
